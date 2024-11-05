378ec0fa1eb782dbd60886f1f6f00adec9b6735c
root <isinsu.katircioglu@gmail.com>
2024-11-05 15:46:29 +0000

    path related changes in undistort

--- scripts/undistort_h5.py
+++ scripts/undistort_h5.py
@@ -22,132 +22,131 @@ parser.add_argument("--h5-chunk-size", type=int, default=24)
 
 
 def process_files(rank, args, file_queue):
-
     while not file_queue.empty():
         try:
             file_path = file_queue.get_nowait()
         except queue.Empty:
             break
+       
+        #try:  # catch all errors
 
-        try:  # catch all errors
+        os.makedirs(
+            os.path.dirname(file_path).replace(args.replace_from, args.replace_to),
+            exist_ok=True,
+        )
+        
+        with h5py.File(file_path, "r") as read_f:
+            with h5py.File(
+                os.path.join(
+                    os.path.dirname(file_path),
+                    f"camera_{os.path.basename(file_path)}",
+                ),
+                "r",
+            ) as read_calib_f:
+                K = read_calib_f["camera"][:]
+                dist = read_calib_f["distortion"][:]
 
-            os.makedirs(
-                os.path.dirname(file_path).replace(args.replace_from, args.replace_to),
-                exist_ok=True,
-            )
-
-            with h5py.File(file_path, "r") as read_f:
-                with h5py.File(
-                    os.path.join(
-                        os.path.dirname(file_path),
-                        f"camera_{os.path.basename(file_path)}",
+            with h5py.File(
+                os.path.join(
+                    os.path.dirname(file_path).replace(
+                        args.replace_from, args.replace_to
                     ),
-                    "r",
-                ) as read_calib_f:
-                    K = read_calib_f["camera"][:]
-                    dist = read_calib_f["distortion"][:]
+                    os.path.basename(file_path),
+                ),
+                "w",
+            ) as write_f:
+                new_K, _ = cv2.getOptimalNewCameraMatrix(
+                    K,
+                    dist,
+                    (args.image_width, args.image_height),
+                    0,
+                    (args.image_width, args.image_height),
+                )
+                mapx, mapy = cv2.initUndistortRectifyMap(
+                    K,
+                    dist,
+                    None,
+                    new_K,
+                    (args.image_width, args.image_height),
+                    5,
+                )
 
                 with h5py.File(
                     os.path.join(
                         os.path.dirname(file_path).replace(
                             args.replace_from, args.replace_to
                         ),
-                        os.path.basename(file_path),
+                        f"camera_{os.path.basename(file_path)}",
                     ),
                     "w",
-                ) as write_f:
-                    new_K, _ = cv2.getOptimalNewCameraMatrix(
-                        K,
-                        dist,
-                        (args.image_size[1], args.image_size[0]),
-                        0,
-                        (args.image_size[1], args.image_size[0]),
-                    )
-                    mapx, mapy = cv2.initUndistortRectifyMap(
-                        K,
-                        dist,
-                        None,
-                        new_K,
-                        (args.image_size[1], args.image_size[0]),
-                        5,
+                ) as write_calib_f:
+                    write_calib_f.create_dataset(
+                        "camera",
+                        data=new_K,
+                        dtype="float32",
                     )
 
-                    with h5py.File(
-                        os.path.join(
-                            os.path.dirname(file_path).replace(
-                                args.replace_from, args.replace_to
-                            ),
-                            f"camera_{os.path.basename(file_path)}",
-                        ),
-                        "w",
-                    ) as write_calib_f:
-                        write_calib_f.create_dataset(
-                            "camera",
-                            data=new_K,
-                            dtype="float32",
-                        )
+                num_written = read_f["num_written"][0]
 
-                    num_written = read_f["num_written"][0]
-
-                    video_ds = write_f.create_dataset(
-                        "video",
-                        (num_written, args.image_height, args.image_width, 3),
-                        chunks=(
-                            min(args.h5_chunk_size, num_written),
-                            args.image_height,
-                            args.image_width,
-                            3,
-                        ),
-                        dtype="uint8",
-                    )
-                    write_f.create_dataset(
-                        "num_written", data=[num_written], dtype="int32"
-                    )
-
-                    read_idx = 0
-                    while read_idx < num_written:
-                        start_idx = read_idx
-                        stop_idx = min(read_idx + args.chunk_size, num_written)
-                        data = read_f["video"][start_idx:stop_idx]
-
-                        # undistort the image
-                        for i, d in enumerate(data):
-                            undistort_d = cv2.remap(d, mapx, mapy, cv2.INTER_LINEAR)
-                            video_ds[start_idx + i] = undistort_d
-
-                        read_idx = stop_idx
-
-                    assert read_idx == num_written
-
-            print(f"Finished processing {file_path}")
-
-        except Exception as e:
-            print(e)
-            print(f"Error processing {file_path}. Moving on.")
-            with open(os.path.join(args.log_dir, "failed_undistort_h5.txt"), "a") as f:
-                f.write(f"{file_path} REASON: {e}\n")
-            try:
-                os.remove(
-                    os.path.join(
-                        os.path.dirname(file_path).replace(
-                            args.replace_from, args.replace_to
-                        ),
-                        os.path.basename(file_path),
-                    )
+                video_ds = write_f.create_dataset(
+                    "video",
+                    (num_written, args.image_height, args.image_width, 3),
+                    chunks=(
+                        min(args.h5_chunk_size, num_written),
+                        args.image_height,
+                        args.image_width,
+                        3,
+                    ),
+                    dtype="uint8",
                 )
-            except OSError:
-                pass
-            try:
-                os.remove(
-                    os.path.join(
-                        os.path.dirname(file_path).replace(
-                            args.replace_from, args.replace_to
-                        ),
-                        f"camera_{os.path.basename(file_path)}",
-                    )
+                write_f.create_dataset(
+                    "num_written", data=[num_written], dtype="int32"
                 )
-            except OSError:
-                pass
+
+                read_idx = 0
+                while read_idx < num_written:
+                    start_idx = read_idx
+                    stop_idx = min(read_idx + args.chunk_size, num_written)
+                    data = read_f["video"][start_idx:stop_idx]
+
+                    # undistort the image
+                    for i, d in enumerate(data):
+                        undistort_d = cv2.remap(d, mapx, mapy, cv2.INTER_LINEAR)
+                        video_ds[start_idx + i] = undistort_d
+
+                    read_idx = stop_idx
+
+                assert read_idx == num_written
+
+        print(f"Finished processing {file_path}")
+
+        # except Exception as e:
+        #     print(e)
+        #     print(f"Error processing {file_path}. Moving on.")
+        #     with open(os.path.join(args.log_dir, "failed_undistort_h5.txt"), "a") as f:
+        #         f.write(f"{file_path} REASON: {e}\n")
+        #     try:
+        #         os.remove(
+        #             os.path.join(
+        #                 os.path.dirname(file_path).replace(
+        #                     args.replace_from, args.replace_to
+        #                 ),
+        #                 os.path.basename(file_path),
+        #             )
+        #         )
+        #     except OSError:
+        #         pass
+        #     try:
+        #         os.remove(
+        #             os.path.join(
+        #                 os.path.dirname(file_path).replace(
+        #                     args.replace_from, args.replace_to
+        #                 ),
+        #                 f"camera_{os.path.basename(file_path)}",
+        #             )
+        #         )
+        #     except OSError:
+        #         pass
 
 
 if __name__ == "__main__":
@@ -155,7 +154,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     file_paths = sorted(Path(args.base_dir).rglob("*.h5"))
-
+    file_paths = [f for f in file_paths if 'camera_' not in str(f)]
+    file_paths = [f for f in file_paths if 'depth_' not in str(f)]
+    file_paths = [f for f in file_paths if 'trajectory_' not in str(f)]
     file_queue = mp.Queue()
     for p in file_paths:
         file_queue.put(p)
